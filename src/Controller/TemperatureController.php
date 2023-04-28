137dc7b03f27fa22082b7338ffb345f15ecca6e6
nailyaphilippo <nailya@sall.com>
2023-04-28 10:45:15 +0300

    temerature

---
+++ src/Controller/TemperatureController.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+
+
+class TemperatureController {
+
+    #[Route('/temp')]
+    public function temperature(
+        Request $request) { 
+    $temp =  $request->query->get('temperature');
+    if (!is_numeric($temp)) {
+
+    // Return an error if the temperature is invalid
+        
+    return new Response("Error: Temperature must be a number", 400);
+        
+        }
+    else {$fahrenheit = ($temp * 9 / 5) + 32;
+    
+    return new Response("The temperature in Fahrenheit: " . $fahrenheit);
+}
+}
+}
